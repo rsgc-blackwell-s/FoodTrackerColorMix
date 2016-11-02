12f86d282f5ad1eee3441881147b8e117c5e3945
Scott Blackwell <student@macbook-pro-blackwell.local>
2016-11-02 14:37:50 -0400

    End of class, saving my work - about to implement two new methods.

--- ColorMix/ViewController.swift
+++ ColorMix/ViewController.swift
@@ -8,14 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
     // MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Handle the text field’s user input through delegate callbacks.
+        nameTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
