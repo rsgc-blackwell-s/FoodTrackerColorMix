1fefa8807853f7b7b41fbe3065e4addb8a0c59ff
Scott Blackwell <student@macbook-pro-blackwell.local>
2016-11-05 19:02:20 -0400

    Added a new subclass!

---
+++ ColorMix/RatingControl.swift
@@ -0,0 +1,14 @@
+//
+//  RatingControl.swift
+//  ColorMix
+//
+//  Created by Scott Blackwell on 2016-11-05.
+//  Copyright © 2016 Scott Blackwell. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+
+}
