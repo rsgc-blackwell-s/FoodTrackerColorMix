60a778a1e62f9bf920e3c083957a8e15bf6e75a4
Scott Blackwell <student@macbook-pro-blackwell.local>
2016-11-05 19:31:05 -0400

    Checkpoint: added a red button into the initializer!

--- ColorMix/RatingControl.swift
+++ ColorMix/RatingControl.swift
@@ -9,6 +9,19 @@
 import UIKit
 
 class RatingControl: UIView {
-
-
+// MARK: Initialization
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+        button.backgroundColor = UIColor.red
+        addSubview(button)
+    }
+    // NOTE: intrinsicSize is now a computed property
+    override public var intrinsicContentSize: CGSize {
+        get {
+            // Return the intrinsic content size for the stack view so it knows how to layout the button
+            return CGSize (width: 240, height: 44)
+        }
+    }
 }
