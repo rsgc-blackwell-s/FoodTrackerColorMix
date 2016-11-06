b32c6d23185b86710c2833bcb329459713f0101c
Scott Blackwell <student@macbook-pro-blackwell.local>
2016-11-06 11:58:05 -0500

    Checkpoint: Created a new class called Meal.

---
+++ ColorMix/Meal.swift
@@ -0,0 +1,31 @@
+//
+//  Meal.swift
+//  ColorMix
+//
+//  Created by Scott Blackwell on 2016-11-06.
+//  Copyright © 2016 Scott Blackwell. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    // MARK: Properties
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    // MARK: Initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+        // Initialization should fail if there is no name or if the rating is negative.
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
+    }
+}
+
