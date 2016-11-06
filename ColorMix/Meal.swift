eb2a1c872cd5b9070cfcc5fb22cb0a10b225a752
Scott Blackwell <student@macbook-pro-blackwell.local>
2016-11-06 18:45:29 -0500

    Checkpoint: Created a file path to data, checking to make sure app builds.

--- ColorMix/Meal.swift
+++ ColorMix/Meal.swift
@@ -8,11 +8,22 @@
 
 import UIKit
 
-public class Meal {
+public class Meal: NSObject, NSCoding {
     // MARK: Properties
     var name: String
     var photo: UIImage?
     var rating: Int
+    
+    // MARK: Archiving Paths
+    static let DocumentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    static let ArchiveURL = DocumentsDirectory[0].appendingPathComponent("meals")
+    
+    // MARK: Types
+    struct PropertyKey {
+        static let nameKey = "name"
+        static let photoKey = "photo"
+        static let ratingKey = "rating"
+    }
     
     // MARK: Initialization
     
@@ -22,10 +33,32 @@ public class Meal {
         self.photo = photo
         self.rating = rating
         
+        super.init()
+        
         // Initialization should fail if there is no name or if the rating is negative.
         if name.isEmpty || rating < 0 {
             return nil
         }
     }
+    
+    // MARK: NSCoding
+    public func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: PropertyKey.nameKey)
+        aCoder.encode(photo, forKey: PropertyKey.photoKey)
+        aCoder.encode(rating, forKey: PropertyKey.ratingKey)
+    }
+    
+    required convenience public init?(coder aDecoder: NSCoder){
+        let name = aDecoder.decodeObject(forKey: PropertyKey.nameKey) as! String
+        
+        // Because photo is an optional property of Meal, use conditional cast.
+        let photo = aDecoder.decodeObject(forKey: PropertyKey.photoKey) as? UIImage
+        
+        let rating = aDecoder.decodeInteger(forKey: PropertyKey.ratingKey)
+        
+        // Must call designated initializer.
+        self.init(name: name, photo: photo, rating: rating)
+    }
+    
 }
 
