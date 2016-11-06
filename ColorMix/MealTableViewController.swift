2af841c80b3efc0c16b9ca92b97d8060eb16b416
Scott Blackwell <student@macbook-pro-blackwell.local>
2016-11-06 16:38:12 -0500

    Checkpoint: the save button now works for adding a new meal to the list.

--- ColorMix/MealTableViewController.swift
+++ ColorMix/MealTableViewController.swift
@@ -110,5 +110,14 @@ class MealTableViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
-    
+   
+    @IBAction func unwindToMealList(sender: UIStoryboardSegue) {
+        if let sourceViewController = sender.source as? MealViewController, let meal = sourceViewController.meal {
+            
+            // Add a new meal.
+            let newIndexPath = IndexPath(row: meals.count, section: 0)
+            meals.append(meal)
+            tableView.insertRows(at: [newIndexPath], with: .bottom)
+        }
+    }
 }
--- ColorMix/MealViewController.swift
+++ ColorMix/MealViewController.swift
@@ -14,6 +14,13 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var ratingControl: RatingControl!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    
+    /*
+     This value is either passed by `MealTableViewController` in `prepareForSegue(_:sender:)`
+     or constructed as part of adding a new meal.
+     */
+    var meal: Meal?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -49,6 +56,22 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
         
         // Dismiss the picker.
         dismiss(animated: true, completion: nil)
+    }
+// MARK: Navigation
+    
+    // This method lets you configure a view controller before it's presented.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let senderObject = sender as AnyObject? {
+            if saveButton === senderObject {
+                let name = nameTextField.text ?? ""
+                let photo = photoImageView.image
+                let rating = ratingControl.rating
+                
+                // Set the meal to be passed to MealTableViewController after the unwind segue.
+                meal = Meal(name: name, photo: photo, rating: rating)
+            }
+        }
+        
     }
 // MARK: Actions
     @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
