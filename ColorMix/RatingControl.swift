e0ed678c3014fa763f051c276bad90131947c5dc
Scott Blackwell <student@macbook-pro-blackwell.local>
2016-11-05 21:24:59 -0400

    Checkpoint: App is still running the same, new variables have been created.

--- ColorMix/RatingControl.swift
+++ ColorMix/RatingControl.swift
@@ -12,21 +12,27 @@ class RatingControl: UIView {
     // MARK: Properties
     var rating = 0
     var ratingButtons = [UIButton]()
+    let spacing = 5
+    let starCount = 5
+    
     // MARK: Initialization
     override func layoutSubviews(){
-        var buttonFrame = CGRect(x: 0, y: 0, width: 44, height: 44)
+        // Set the button's width and height to a square the size of the frame's height.
+        let buttonSize = Int(frame.size.height)
+        var buttonFrame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
         
         // Offset each button's origin by the length of the button plus spacing.
         for (index, button) in ratingButtons.enumerated(){
-            buttonFrame.origin.x = CGFloat(index * (44+5))
+            buttonFrame.origin.x = CGFloat(index * (buttonSize + spacing))
             button.frame = buttonFrame
         }
     }
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
-        for _ in 0..<5 {
-        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+        for _ in 0..<starCount {
+        let button = UIButton()
         button.backgroundColor = UIColor.red
         button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchDown)
         ratingButtons += [button]
@@ -36,11 +42,12 @@ class RatingControl: UIView {
     
     // NOTE: intrinsicSize is now a computed property
     override public var intrinsicContentSize: CGSize {
-        get {
+            let buttonSize = Int(frame.size.height)
+            let width = (buttonSize * starCount) = (spacing * (starCount - 1))
             // Return the intrinsic content size for the stack view so it knows how to layout the button
-            return CGSize (width: 240, height: 44)
-        }
+            return CGSize (width: width, height: buttonSize)
     }
+    
     // MARK: Button Action
     func ratingButtonTapped (button: UIButton){
         print ("Button pressed 👍")
