8677512cfbe5e31b144cbeaa99573e9feb07b96d
Scott Blackwell <student@macbook-pro-blackwell.local>
2016-11-05 22:31:25 -0400

    Checkpoint: The stars should be clickable and change fill based on user interaction... buttons are still too large for me, but the app doesn't crash at least!

--- ColorMix/RatingControl.swift
+++ ColorMix/RatingControl.swift
@@ -14,19 +14,13 @@ class RatingControl: UIView {
     var ratingButtons = [UIButton]()
     let spacing = 5
     let starCount = 5
-    
-    // MARK: Initialization
-    override func layoutSubviews(){
-        // Set the button's width and height to a square the size of the frame's height.
-        let buttonSize = Int(frame.size.height)
-        var buttonFrame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
-        
-        // Offset each button's origin by the length of the button plus spacing.
-        for (index, button) in ratingButtons.enumerated(){
-            buttonFrame.origin.x = CGFloat(index * (buttonSize + spacing))
-            button.frame = buttonFrame
+    var rating = 0 {
+        didSet {
+            setNeedsLayout()
         }
     }
+    
+    // MARK: Initialization
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -51,6 +45,19 @@ class RatingControl: UIView {
         }
     }
     
+    override func layoutSubviews(){
+        // Set the button's width and height to a square the size of the frame's height.
+        let buttonSize = Int(frame.size.height)
+        var buttonFrame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
+        
+        // Offset each button's origin by the length of the button plus spacing.
+        for (index, button) in ratingButtons.enumerated(){
+            buttonFrame.origin.x = CGFloat(index * (buttonSize + 5))
+            button.frame = buttonFrame
+        }
+        updateButtonSelectionStates()
+    }
+    
     // NOTE: intrinsicSize is now a computed property
     override public var intrinsicContentSize: CGSize {
             let buttonSize = Int(frame.size.height)
@@ -62,5 +69,15 @@ class RatingControl: UIView {
     // MARK: Button Action
     func ratingButtonTapped (button: UIButton){
         print ("Button pressed 👍")
+        rating = ratingButtons.index(of: button)! + 1
+        
+        updateButtonSelectionStates()
+    }
+    
+    func updateButtonSelectionStates() {
+        for (index, button) in ratingButtons.enumerated(){
+            // If the index of a button is less than the rating, that button should be selected.
+            button.isSelected = index < rating
+        }
     }
 }
