9d6b91d606a3ee37844b5a6770e70e7c64c0fe0e
Scott Blackwell <student@macbook-pro-blackwell.local>
2016-11-05 20:47:23 -0400

    Checkpoint: Adding four more buttons to make a total of five, though, the buttons are stacked on top of each other at the moment... this will be fixed soon.

--- ColorMix/RatingControl.swift
+++ ColorMix/RatingControl.swift
@@ -9,14 +9,19 @@
 import UIKit
 
 class RatingControl: UIView {
-// MARK: Initialization
+    // MARK: Properties
+    var rating = 0
+    var ratingButtons = [UIButton]()
+    // MARK: Initialization
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        
+        for _ in 0..<5 {
         let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
         button.backgroundColor = UIColor.red
         button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchDown)
+        ratingButtons += [button]
         addSubview(button)
+        }
     }
     
     // NOTE: intrinsicSize is now a computed property
